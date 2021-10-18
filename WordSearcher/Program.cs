2631934552c4f0950e7609b18585018eb30f5ede
Viktor Salmberg <71824815+Norrlandsk@users.noreply.github.com>
2021-10-18 09:14:03 +0200

    test

--- WordSearcher/Program.cs
+++ WordSearcher/Program.cs
@@ -2,11 +2,11 @@
 {
     using System;
 
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!!");
         }
     }
 }
