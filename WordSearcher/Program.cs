69d49097505a35ebb9d222f363463cda121d3502
Elias Hjelm <hjelmarn@protonmail.com>
2021-10-18 11:55:46 +0200

    Created ReadFiles method

--- WordSearcher/Program.cs
+++ WordSearcher/Program.cs
@@ -1,12 +1,15 @@
 ﻿namespace WordSearcher
 {
     using System;
+    using WordSearcher.TextFile;
 
     internal class Program
     {
         private static void Main(string[] args)
         {
             Console.WriteLine("Hello World!!!!!!!");
+            FileHandler.ReadFiles();
+            
         }
     }
 }
--- WordSearcher/TextFile/FileHandler.cs
+++ WordSearcher/TextFile/FileHandler.cs
@@ -1,10 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Net;
 using System.Text;
 
 namespace WordSearcher.TextFile
 {
-    class FileHandler
+    internal class FileHandler
     {
+        public static string textOne;
+        public static string textTwo;
+        public static string textThree;
+
+        public static void ReadFiles()
+        {
+            string docPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+            textOne = System.IO.File.ReadAllText(Path.Combine(docPath, "Text_1.txt"));
+            textTwo = System.IO.File.ReadAllText(@"C:\Users\Studera\source\repos\WordSearcher\WordSearcher\TextFiles\Text_2.txt");
+            textThree = System.IO.File.ReadAllText(@"C:\Users\Studera\source\repos\WordSearcher\WordSearcher\TextFiles\Text_3.txt");
+
+            Console.WriteLine(textOne);
+            //Console.WriteLine(textTwo);
+            //Console.WriteLine(textThree);
+
+        }
+
+
+
+
+
+        
     }
 }
